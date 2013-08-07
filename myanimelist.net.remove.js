462c9b649f9aa475dea40f47ff12b7145b0b7c19
Wing Gao <wing.gao@live.com>
2013-08-07 12:11:55 +0800

    modify the context

--- myanimelist.net.remove.js
+++ myanimelist.net.remove.js
@@ -4,19 +4,34 @@
 // @version    0.1
 // @description  enter something useful
 // @include http://myanimelist.net/anime.php*
-// @match      http://*/*
 // @copyright  2013, wing
 // ==/UserScript==
 
 
 function main()
 {
+	var srPath = "//*[@id='content']/div[2]/table/tbody/tr[1]/td[1]";
+    var sr = document.evaluate(srPath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+    
+    var btn = document.createElement("button");
+	btn.innerHTML = "Remove Viewed";
+	btn.onclick = function(){remove();};	
+	sr.appendChild(btn);
+}
+function remove()
+{
 	var q = "//a[@title='Edit this entry']";
 	var xps = document.evaluate(q, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
     var a = xps.iterateNext();
+	var nod = new Array()
 	while(a)
 	{
-		a.parentNode.parentNode.remove();
+		nod.push(a);
+		a = xps.iterateNext();
+	}
+	for(var i=0;i<nod.length;i++)
+	{
+		nod[i].parentNode.parentNode.remove();
 	}
 }
 main()
