ed73061bf446dd9fd5d8257fb14e4e15b99b9281
Wing Gao <wing.gao@live.com>
2014-05-18 22:16:37 +0800

    添加搜索

--- myanimesearch.js
+++ myanimesearch.js
@@ -6,31 +6,29 @@
 // @include        http://myanimelist.net/manga/*
 // ==/UserScript==
 var searchs = [
-    {name: 'btdigg', short: 'bt', href: 'http://btdigg.org/search?q=%s'},
-    {name: 'bitsnoop', short: 'bs', href: 'http://bitsnoop.com/search/all/%s'}
+    {name: 'btdigg', short: 'BT', href: 'http://btdigg.org/search?q=%s'},
+    {name: 'bitsnoop', short: 'BS', href: 'http://bitsnoop.com/search/all/%s'},
+    {name: 'NyaaTorrents', short: 'Nyaa', href: 'http://www.nyaa.se/?page=search&term=%s'},
+    {name: 'KickAss', short: 'KA', href: 'http://kickass.to/usearch/%s'},
+    {name: 'Google', short: 'GG', href: 'https://www.google.co.jp/search?q=%s+torrent&ie=UTF-8'}
 ];
 function getTitle(item) {
     var txt = item.innerText;
     var index = txt.indexOf(':');
-    if (index >= 0)
-        return txt.substr(index + 1);
-    else return undefined;
+    return txt.substr(index + 1);
 }
-function addSearch(item) {
+function addSearch(item, title) {
     var link = document.createElement('div');
-    var title = getTitle(item);
-    if (title) {
-        for (var i = 0; i < searchs.length; i++) {
-            var s = searchs[i];
-            var a = document.createElement('a');
-            a.target = '_blank';
-            a.href = s.href.replace('%s', title);
-            a.title = s.name;
-            a.innerText = s.short;
-            link.appendChild(document.createTextNode('['));
-            link.appendChild(a);
-            link.appendChild(document.createTextNode('] '));
-        }
+    for (var i = 0; i < searchs.length; i++) {
+        var s = searchs[i];
+        var a = document.createElement('a');
+        a.target = '_blank';
+        a.href = s.href.replace('%s', title);
+        a.title = s.name;
+        a.innerText = s.short;
+        link.appendChild(document.createTextNode('['));
+        link.appendChild(a);
+        link.appendChild(document.createTextNode('] '));
     }
     item.appendChild(link);
 }
@@ -39,9 +37,10 @@ function getItems() {
     var divs = document.evaluate(path, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
     var div;
     for (var i = 0; div = divs.snapshotItem(i); i++) {
-        addSearch(div);
-//        console.log(div);
+        addSearch(div, getTitle(div));
     }
+    var al = document.getElementsByTagName('h2')[0];
+    addSearch(al, document.title.substr(0, document.title.lastIndexOf('-')));
 }
 
 function main() {
