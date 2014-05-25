65964a1fe43766e277ef2d6d1b5c2b634a1c37e9
Wing Gao <wing.gao@live.com>
2014-05-26 01:44:41 +0800

    add pycollection

--- myanimesearch.js
+++ myanimesearch.js
@@ -10,7 +10,8 @@ var searchs = [
     {name: 'bitsnoop', short: 'BS', href: 'http://bitsnoop.com/search/all/%s'},
     {name: 'NyaaTorrents', short: 'Nyaa', href: 'http://www.nyaa.se/?page=search&term=%s'},
     {name: 'KickAss', short: 'KA', href: 'http://kickass.to/usearch/%s'},
-    {name: 'Google', short: 'GG', href: 'https://www.google.co.jp/search?q=%s+torrent&ie=UTF-8'}
+    {name: 'Google', short: 'GG', href: 'https://www.google.co.jp/search?q=%s+torrent&ie=UTF-8'},
+    {name: 'PPC', short: 'PPC', href: 'http://localhost/ppc/item/add?title=%t&url=%u&type=anime&img=%i'}
 ];
 function getTitle(item) {
     var txt = item.innerText;
@@ -20,18 +21,24 @@ function getTitle(item) {
 function addSearch(item, title) {
     var link = document.createElement('div');
     for (var i = 0; i < searchs.length; i++) {
-        var s = searchs[i];
-        var a = document.createElement('a');
-        a.target = '_blank';
-        a.href = s.href.replace('%s', title);
-        a.title = s.name;
-        a.innerText = s.short;
-        link.appendChild(document.createTextNode('['));
-        link.appendChild(a);
-        link.appendChild(document.createTextNode('] '));
+        createSearchItem(link, searchs[i], title);
     }
     item.appendChild(link);
 }
+
+function createSearchItem(link, sitem, title) {
+    var s = sitem;
+    var a = document.createElement('a');
+    a.target = '_blank';
+    if (s.short == 'PPC')a.href = getPPC(s.href, title);
+    else a.href = s.href.replace('%s', title);
+    a.title = s.name;
+    a.innerText = s.short;
+    link.appendChild(document.createTextNode('['));
+    link.appendChild(a);
+    link.appendChild(document.createTextNode('] '));
+}
+
 function getItems() {
     var path = "//div[@class='spaceit_pad' and ./span[@class='dark_text']]";
     var divs = document.evaluate(path, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -43,6 +50,12 @@ function getItems() {
     addSearch(al, document.title.substr(0, document.title.lastIndexOf('-')));
 }
 
+function getPPC(href, title) {
+    var img = document.evaluate('//*[@id="content"]/table/tbody/tr/td[1]/div[1]/a/img');
+    img = img.iterateNext();
+    return href.replace('%t', encodeURIComponent(title)).replace('%u', encodeURIComponent(window.location.href))
+        .replace('%i', encodeURIComponent(img.src));
+}
 function main() {
     getItems();
 }
