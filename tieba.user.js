6c6404d168ebcadf59cc6cef52ef52daf1a39e12
Wing Gao <wing.gao@live.com>
2015-10-20 13:04:15 +0800

    删除楼主回复、来自

--- tieba.user.js
+++ tieba.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       贴吧 — 只显示楼主 精简界面
 // @namespace  https://github.com/WingGao/MyUserScript
-// @version    0.2
+// @version    0.3
 // @description  用于贴吧 【只看楼主】 页面，清理无用部分。
 // @match      http://tieba.baidu.com/p/*?see_lz=1*
 // @copyright  2015+, WingGao
@@ -14,7 +14,9 @@ function clearpage() {
     //举报按钮
     $(".j_jb_ele").remove();
     //回复按钮
-    $(".j_lzl_r").remove();
+    $(".p_reply").remove();
+    //来自xxx
+    $('.post-tail-wrap .tail-info a').parent().remove();
     //回复
     $(".j_lzl_container").remove();
 
