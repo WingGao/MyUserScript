901133c48b0f09e9ddce9e017345c5d939c6b1df
Wing Gao <wing.gao@live.com>
2017-03-05 19:03:08 +0800

    添加样式

--- dm733.user.js
+++ dm733.user.js
@@ -2,17 +2,19 @@
 // @name       733dm查看利器
 // @namespace  https://github.com/WingGao/MyUserScript
 // @require http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
-// @version    1.2
+// @version    1.3
 // @description  方便看动漫
 // @match      http://www.733dm.net/mh/*
 // @copyright  2017+, WingGao
 // ==/UserScript==
 
 $("<style type='text/css'> \
+a:active, a:visited {color: red;}\
 .wing-div {display: inline-block;box-sizing:border-box;width: 45%;padding: 3%;} \
 </style>").appendTo("head");
 $("<div/>").addClass("redbold").text("SOME NEW TEXT").appendTo("body");
 $(document).ready(function () {
+    if(photosr == null)return;
     var htmlstr = '';
     for (var i = 0; i < photosr.length; i++) {
         var img = 'http://733dm.zgkouqiang.cn/' + photosr[i];
