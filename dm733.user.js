e8df1eb3280a69f4afd88a9c5e0ce3fe172b9f9a
Wing Gao <wing.gao@live.com>
2017-03-05 21:39:55 +0800

    添加点击样式

--- dm733.user.js
+++ dm733.user.js
@@ -2,7 +2,7 @@
 // @name       733dm查看利器
 // @namespace  https://github.com/WingGao/MyUserScript
 // @require http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
-// @version    1.3
+// @version    1.4
 // @description  方便看动漫
 // @match      http://www.733dm.net/mh/*
 // @copyright  2017+, WingGao
@@ -14,12 +14,15 @@ a:active, a:visited {color: red;}\
 </style>").appendTo("head");
 $("<div/>").addClass("redbold").text("SOME NEW TEXT").appendTo("body");
 $(document).ready(function () {
-    if(photosr == null)return;
+    //修改快捷方式
+    $('.plist.pmedium a').attr('target', '_blank');
+    //修改样式
+    if (photosr == null)return;
     var htmlstr = '';
     for (var i = 0; i < photosr.length; i++) {
         var img = 'http://733dm.zgkouqiang.cn/' + photosr[i];
         console.log('http://733dm.zgkouqiang.cn/' + photosr[i]);
-        htmlstr += '<div class="wing-div"><div>'+(i+1)+'</div><img src="' + img + '"></div>';
+        htmlstr += '<div class="wing-div"><div>' + (i + 1) + '</div><img src="' + img + '"></div>';
     }
     document.body.innerHTML = htmlstr;
 });
