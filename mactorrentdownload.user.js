42e821bc83df3041653c857c7daad46699bdcdf0
Wing Gao <wing.gao@live.com>
2018-04-17 13:27:17 +0800

    up

--- mactorrentdownload.user.js
+++ mactorrentdownload.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name       mac-torrent-download.net 去广告
 // @namespace  https://github.com/WingGao/MyUserScript
-// @version    1.1
+// @version    1.2
 // @description  mac-torrent-download.net 去广告
-// @match      http://mac-torrent-download.net/*
-// @copyright  2017+, WingGao
+// @match      *://mac-torrent-download.net/*
+// @copyright  2018+, WingGao
 // ==/UserScript==
 
 
 setInterval(function () {
     document.onclick = null;
 }, 200);
+$('a').each((i, v) => { var vq = $(v); if (vq.width() >= window.innerWidth * 0.9) { vq.remove(); }; });
