a723edd7a4169a73ba1db98d350e6cb786dd9ec9
Wing Gao <wing.gao@live.com>
2019-05-13 11:16:13 +0800

    remove jq

--- mtyun.user.js
+++ mtyun.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         美团云优化
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       WingGao
 // @updateURL    https://github.com/WingGao/MyUserScript/raw/master/mtyun.user.js
 // @match        https://www.mtyun.com/login?u-login
 // @grant        none
-// @require https://cdn.staticfile.org/jquery/3.4.1/jquery.slim.min.js
-// @require https://github.com/WingGao/MyUserScript/raw/master/utils.js
+// @require      https://github.com/WingGao/MyUserScript/raw/master/utils.js
 // ==/UserScript==
 
 (function() {
