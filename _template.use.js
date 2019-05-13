327631876438c0a5f48c9f1dde1ff9d327ee7b7f
Wing Gao <wing.gao@live.com>
2019-05-13 11:13:27 +0800

    add update

--- _template.use.js
+++ _template.use.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       WingGao
 // @match        https://www.mtyun.com/login?u-login
+// @updateURL    https://github.com/WingGao/MyUserScript/raw/master/.js
 // @grant        none
 // @require https://cdn.staticfile.org/jquery/3.4.1/jquery.slim.min.js
 // @require https://github.com/WingGao/MyUserScript/raw/master/utils.js
--- mtyun.user.js
+++ mtyun.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       WingGao
+// @updateURL    https://github.com/WingGao/MyUserScript/raw/master/mtyun.user.js
 // @match        https://www.mtyun.com/login?u-login
 // @grant        none
 // @require https://cdn.staticfile.org/jquery/3.4.1/jquery.slim.min.js
