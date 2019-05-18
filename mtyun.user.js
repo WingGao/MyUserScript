0e0a2e5c296e72c2117594408156e7edda2fadbc
Wing Gao <wing.gao@live.com>
2019-05-18 15:20:44 +0800

    update url

--- mtyun.user.js
+++ mtyun.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         美团云优化
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  try to take over the world!
+// @version      0.4
+// @description  美团云优化
 // @author       WingGao
 // @updateURL    https://github.com/WingGao/MyUserScript/raw/master/mtyun.user.js
-// @match        https://www.mtyun.com/login?u-login
+// @match        https://www.mtyun.com/login?*
 // @grant        none
 // @require      https://github.com/WingGao/MyUserScript/raw/master/utils.js
 // ==/UserScript==
 
-(function() {
-    WingWaitElement('#unitive-login-iframe',(jqIframe)=>{
+(function () {
+    WingWaitElement('#unitive-login-iframe', (jqIframe) => {
         $('.login-content-right').append(`<a href='${jqIframe.attr('src')}'>login</a>`);
     })
 })();
